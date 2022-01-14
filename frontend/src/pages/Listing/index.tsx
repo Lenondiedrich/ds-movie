db5c4b75187a0ed77ef9136b2b05a5d18a335064
Lenon <lenon.diedrich@gmail.com>
2022-01-14 19:11:23 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,16 @@
 import { Box, Grid } from "@chakra-ui/react";
+import axios from "axios";
 import { Movie, MovieCard } from "../../components/MovieCard";
 import { Pagination } from "../../components/Pagination";
 import { movies } from "../../config/constants";
+import { BASE_URL } from "../../utils/requests";
 
 export const Listing = () => {
+  //FORMA ERRADA
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Box backgroundColor="#000" p="30px" w="100%">
